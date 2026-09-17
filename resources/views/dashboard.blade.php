5722b434cd5269857d96f1bebc2816a471eff039
Muhammad Uzair <uzairupwork141@gmail.com>
2026-09-17 23:09:58 +0500

    Install and configure AdminLTE

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.admin')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+
+<div class="row">
+
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>0</h3>
+                <p>Medicines</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-pills"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>0</h3>
+                <p>Customers</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-users"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-warning">
+            <div class="inner">
+                <h3>0</h3>
+                <p>Sales</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-shopping-cart"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-danger">
+            <div class="inner">
+                <h3>0</h3>
+                <p>Low Stock</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-exclamation-triangle"></i>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+@stop
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+
+@section('title', 'Medical Management System')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+
+    <div class="card">
+        <div class="card-body">
+            <h3>Welcome to Medical Management System</h3>
+            <p>AdminLTE is working successfully.</p>
+        </div>
+    </div>
+
+@stop
